3d2aa0d49c658bc1c1b6c726f969bb9ed9dac022
Rabiul Islam Apu (Neel) <rabiul17.ri@gmail.com>
2024-04-07 02:42:18 +0600

    Add files via upload

---
+++ Basics & Variables-JavaScripts/One.js
@@ -0,0 +1,67 @@
+//print function
+console.log("Lets Start JavScript");
+
+//integer, float or any variable
+age = 17.5;
+console.log(age);
+
+//if else condition
+if (age >= 18) {
+    console.log("You are eligible to vote");
+} else {
+    console.log("You are not eligible to vote");
+}
+
+
+//string variable
+name = "Tony Stark";
+console.log(name);
+
+
+//Null value
+var age = null;
+console.log(age);
+
+//undefined value
+y = undefined;
+console.log(y); 
+
+
+
+//boolean variable
+isFollower = false;
+console.log(isFollower);
+isFollower = true;
+console.log(isFollower);
+
+//var let const variable
+//var is changeable
+var age = 24;
+var age = 54;
+var age = 60;
+console.log(age);
+
+//let let fixed the variable but can be update later
+//let age = 24;
+//age = 54;
+//console.log(age);
+
+//cons fixed the variable
+//const age = 24;
+//console.log(age);
+
+
+//block code isolated from the wghole code
+{
+    let age = 55;
+    age = 20;
+    console.log(age);
+}
+
+//data type
+//number. string, boolean, undefined, null, number
+let price = 100;
+console.log(price);
+// Big int
+y = BigInt("300");
+console.log(y); // will show n in the last
